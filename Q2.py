b39483ba694e40c83fd3d553f3c0a44fddfc5398
Amitgaikwad45 <48085448+Amitgaikwad45@users.noreply.github.com>
2021-05-11 17:41:34 +0530

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -3,10 +3,12 @@ total = 0
 
 
 # creating a list
-list1 = [1,2,-8,5.5]
+list1 = [11,5,17,18,23,'lemon',"783",1.0]
 
-    for x in list1:
+for x in list1:
+    if x.__class__.__name__ in["int","float"]:
         total += x
+    continue
 
 
 print(total)
