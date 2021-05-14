3a8d8fede08d244713ba1430c182c3d3db5cfc25
Amitgaikwad45 <48085448+Amitgaikwad45@users.noreply.github.com>
2021-05-14 15:01:27 +0530

    Update test_login.py

--- Pytest/test_login.py
+++ Pytest/test_login.py
@@ -1,1 +1,15 @@
+#implementation of pytest
+import pytest
 
+@pytest.fixture()
+def setup():
+    print("opening url")
+    yield
+    print("closing browser")
+
+
+def test_loginbyMail(setup):
+    print("mail login")
+
+def test_loginbyFacebook(setup):
+    print("facebook login")
