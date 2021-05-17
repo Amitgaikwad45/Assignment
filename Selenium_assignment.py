4fa769294c2170125ecad98fc868cd13ab41d2e9
Amitgaikwad45 <48085448+Amitgaikwad45@users.noreply.github.com>
2021-05-17 13:24:13 +0530

    Update Selenium_assignment.py

--- Selenium_assignment.py
+++ Selenium_assignment.py
@@ -1,25 +1,35 @@
-#program 1
 from selenium import webdriver
 import time
+from selenium.common.exceptions import NoSuchElementException
+
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 driver = webdriver.Chrome(executable_path='F:/chromedriver.exe')
-
-# Go to www.google.com
-driver.get("https://www.google.com")
-
+driver.get("http://www.google.com")
 driver.maximize_window()
-driver.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input").send_keys("selenium hq")
-#driver.find_element(By.NAME,"btnK").click()
-driver.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]").click()
+try:
+    element = driver.find_element_by_name("q");
+    element.send_keys("selenium hq");
+    element.submit();
 
-element=driver.find_element_by_xpath("//*[@id='rso']/div[8]/div/div/div[2]/span/span").text
-#element=driver.find_element_by_id('rso').text
-print(element)
-
-
+except:
+    print("not found")
 
 
+f=0
+for r in range(1,10):
+    results = driver.find_elements_by_xpath("//h3[text()='Selenium (software) - Wikipedia']")
 
-
+    for re in results:
+        if(re.text=='Selenium (software) - Wikipedia'):
+            #r.click()
+            ele=driver.find_element_by_xpath("//div[@class='IsZvec']/span[@class='aCOpRe']/span[contains(text(),'is a portable frame')]")
+            print(ele.text)
+            f=1
+            break
+    if(f==1):
+        break
+    else:
+        driver.find_element_by_xpath("// *[ @ id = 'pnnext'] / span[2]").click()
+driver.close()
 
