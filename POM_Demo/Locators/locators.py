5c226843d894dbfc48e4ea7661b7d00cdf0637c5
Amitgaikwad45 <48085448+Amitgaikwad45@users.noreply.github.com>
2021-05-18 15:40:17 +0530

    Update locators.py

--- POM_Demo/Locators/locators.py
+++ POM_Demo/Locators/locators.py
@@ -1,1 +1,13 @@
 
+
+class Locators():
+
+    #Login page objects
+    username_textbox_id = "txtUsername"
+    password_textbox_id = "txtPassword"
+    login_button_id = "btnLogin"
+
+    #Home page objects
+    welcome_link_id = "welcome"
+    logout_link_text = "Logout"
+    path = "C:/Users/amit.g/Pictures/Saved Pictures/page.png"
