d2ae127af5e9fd30a24ac066bf48211ecdc2077b
Amitgaikwad45 <48085448+Amitgaikwad45@users.noreply.github.com>
2021-05-26 12:47:23 +0530

    Update Locators.py

--- robotframework/PageObjects/Locators.py
+++ robotframework/PageObjects/Locators.py
@@ -2,3 +2,13 @@
 txt_loginUserName="name:txtUsername"
 txt_loginPassword="name:txtPassword"
 btn_login="xpath://input[@id='btnLogin']"
+
+
+#Registration page elements for RegistrationKeywords.robot
+txt_gender="name:Gender"
+txt_fnm="name:FirstName"
+txt_lnm="name:LastName"
+txt_email="name:Email"
+txt_pwd="name:Password"
+txt_cpwd="name:ConfirmPassword"
+btn_reg="xpath://input[@id='register-button']"
