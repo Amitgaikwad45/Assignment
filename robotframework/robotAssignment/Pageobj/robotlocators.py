09e991cbe06aae9351546c9438fb3e963f64ae68
Amitgaikwad45 <48085448+Amitgaikwad45@users.noreply.github.com>
2021-05-29 10:07:01 +0530

    Create robotlocators.py

---
+++ robotframework/robotAssignment/Pageobj/robotlocators.py
@@ -0,0 +1,37 @@
+#locators for robotass_keywords
+
+btn_signin="xpath://*[@id='app']/div/div/div[2]/div/div[2]/button"
+txt_email="xpath://*[@id='identifierId']"
+btn_nxt="xpath://*[@id='identifierNext']/div/button"
+
+txt_pwd="xpath://*[@id='password']/div[1]/div/div[1]/input"
+
+btn_nxt_pwd="xpath://*[@id='passwordNext']/div/button"
+link_txt="xpath://*[@id='view_container']/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div/div/ul/li[1]/div/div[1]/div"
+btn_allow="xpath://*[@id='submit_approve_access']/div/button"
+
+#Fetch all options
+dboard="xpath://div/div/p[text()='Dashboard']"
+skill="xpath://div/div/p[text()='Skills']"
+tsheet="xpath://div/div/p[text()='Timesheet']"
+reprt="xpath://div/div/p[text()='Reports']"
+prs="xpath://div/div/p[text()='PRS']"
+ta="xpath://div/div/p[text()='Training Assessment']"
+lcm="xpath://div/div/p[text()='LHCM']"
+Tms="xpath://div/div/p[text()='TMS']"
+out_lg="xpath:/html/body/div[2]/div[3]/div/div[2]/ul[2]/a/div"
+
+#Entering TMS section
+raise_tkt="xpath:/html/body/app-root/app-navbar/mat-sidenav-container/mat-sidenav/mat-nav-list/a[1]/div"
+history="xpath:/html/body/app-root/app-navbar/mat-sidenav-container/mat-sidenav/mat-nav-list/a[2]/div/span"
+
+
+
+
+
+
+
+
+#erp portal
+nav_ele="xpath://button[@type='button']"
+btn_logout="xpath:/html/body/div[2]/div[3]/div/div[2]/ul[2]/a/div"
