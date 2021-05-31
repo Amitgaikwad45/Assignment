3555c54909bf02a21059041c3de00e2bcfcfcdbb
Amitgaikwad45 <48085448+Amitgaikwad45@users.noreply.github.com>
2021-05-31 15:23:05 +0530

    Update Locators.py

--- robotframework/PageObjects/Locators.py
+++ robotframework/PageObjects/Locators.py
@@ -12,3 +12,12 @@ txt_email="name:Email"
 txt_pwd="name:Password"
 txt_cpwd="name:ConfirmPassword"
 btn_reg="xpath://input[@id='register-button']"
+
+
+
+#Raise ticket section
+txt_title="xpath://div/input[@id='mat-input-0']"
+txt_des="xpath://div/input[@id='mat-input-1']"
+#select_arrow="xpath://*[@id='mat-select-0']/div/div[2]/div"
+#ticket_type="xpath://*[@id='mat-select-0']/div/div[1]"
+#soft_type="xpath://*[@id='mat-option-8']/span"
