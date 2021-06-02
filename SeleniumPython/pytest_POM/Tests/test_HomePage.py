0a61d5b3f1f74227216da90d65290b66caef2e56
Amitgaikwad45 <48085448+Amitgaikwad45@users.noreply.github.com>
2021-06-02 18:04:37 +0530

    Update test_HomePage.py

--- SeleniumPython/pytest_POM/Tests/test_HomePage.py
+++ SeleniumPython/pytest_POM/Tests/test_HomePage.py
@@ -1,1 +1,23 @@
 
+from Config.config import TestData
+from Pages.LoginPage import LoginPage
+from Tests.test_base import BaseTest
+
+
+class Test_Home(BaseTest):
+    def test_home_page_title(self):
+        self.loginPage=LoginPage(self.driver)
+        homePage=self.loginPage.do_login(TestData.USER_NAME,TestData.PASSWORD)
+        title=homePage.get_home_page_title(TestData.LOGIN_PAGE_TITLE)
+        assert  title == TestData.LOGIN_PAGE_TITLE
+
+    def test_home_page_header(self):
+        self.loginPage = LoginPage(self.driver)
+        homePage = self.loginPage.do_login(TestData.USER_NAME, TestData.PASSWORD)
+        header = homePage.get_header_value()
+        assert header == TestData.HOME_PAGE_HEADER
+
+    def test_user_icon(self):
+        self.loginPage = LoginPage(self.driver)
+        homePage = self.loginPage.do_login(TestData.USER_NAME, TestData.PASSWORD)
+        assert homePage.is_user_icon_exist()
