b601b0eb568b7b37645454a96056e59e068a60de
Amitgaikwad45 <48085448+Amitgaikwad45@users.noreply.github.com>
2021-06-02 16:30:57 +0530

    Create config.py

---
+++ SeleniumPython/pytest_POM/Config/config.py
@@ -0,0 +1,6 @@
+class TestData:
+    CHROME_EXECUTABLE_PATH="E:/chromedriver.exe"
+    BASE_URL="https://opensource-demo.orangehrmlive.com/index.php/auth/login"
+    USER_NAME="Admin"
+    PASSWORD="admin123"
+    LOGIN_PAGE_TITLE="OrangeHRM"
