2b322561da4e1e630583d1e41284d2888a30f9c9
Amitgaikwad45 <48085448+Amitgaikwad45@users.noreply.github.com>
2021-06-03 13:09:27 +0530

    Update HomePage.py

--- SeleniumPython/pytest_POM/Pages/HomePage.py
+++ SeleniumPython/pytest_POM/Pages/HomePage.py
@@ -1,5 +1,5 @@
 
-from Config.config import TestData
+
 from Pages.BasePage import BasePage
 from selenium.webdriver.common.by import By
 
@@ -11,7 +11,7 @@ class HomePage(BasePage):
 
     def __init__(self,driver):
         super().__init__(driver)
-        #self.driver.get(TestData.BASE_URL)
+        
 
     def get_home_page_title(self,title):
         return self.get_title(title)
