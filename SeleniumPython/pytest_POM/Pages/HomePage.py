6389aa263addd6be1a949aae5e5c17b8ea091aa2
Amitgaikwad45 <48085448+Amitgaikwad45@users.noreply.github.com>
2021-06-08 15:40:09 +0530

    Update HomePage.py

--- SeleniumPython/pytest_POM/Pages/HomePage.py
+++ SeleniumPython/pytest_POM/Pages/HomePage.py
@@ -10,19 +10,24 @@ class HomePage(BasePage):
 
 
     def __init__(self,driver):
+        #calling parent class constructor
         super().__init__(driver)
         
 
     def get_home_page_title(self,title):
+        #method returns title of the page
         return self.get_title(title)
 
     def is_user_icon_exist(self):
+        # return true if icon exist else return false
         return self.is_visible(self.UID)
 
     def get_header_value(self):
+        #checks the element is visible or not if visibile return text associated with that element
         if  self.is_visible(self.HEADER):
             return self.get_element_text(self.HEADER)
 
     def get_marketplace_value(self):
+    # checks the element is visible or not if visibile return text associated with that element
         if  self.is_visible(self.MARKETPLACE):
             return self.get_element_text(self.MARKETPLACE)
